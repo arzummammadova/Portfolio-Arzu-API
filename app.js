378d28e76ec42279fae6c95f8e5890412576a28d
arzummammadova <arzuam-af106@code.edu.az>
2025-07-30 23:51:48 +0400

    new

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:3000',
   'https://portfolio-arzu-zeru.vercel.app',
- 'https://arzumammadova.vercel.app/'
+ 'https://arzumammadova.vercel.app'
 ];
 
 app.use(cors({
